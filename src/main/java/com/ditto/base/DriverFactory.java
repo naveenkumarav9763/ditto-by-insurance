f0f0e33ad3691573eabd150440370c54de3499bc
naveenkumarav9763 <naveenkumarav9763@gmail.com>
2026-02-25 17:29:14 +0530

    CI changes

--- src/main/java/com/ditto/base/DriverFactory.java
+++ src/main/java/com/ditto/base/DriverFactory.java
@@ -2,29 +2,48 @@ package com.ditto.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverFactory {
 
 	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	
+
 	public DriverFactory() {
 	}
 
+	private static boolean isCIEnvironment() {
+		String envProperty = System.getProperty("env");
+		String ciEnvVariable = System.getenv("CI");
+
+		return "ci".equalsIgnoreCase(envProperty)
+				|| "true".equalsIgnoreCase(ciEnvVariable);
+	}
+
 	public static void initDriver(String browser) {
+		boolean isCI = isCIEnvironment();
 		switch (browser.toLowerCase()) {
-		case "chrome":
-			DriverFactory.driver.set(new ChromeDriver());
-			break;
-		case "firefox":
-			DriverFactory.driver.set(new FirefoxDriver());
-			break;
-		case "edge":
-			DriverFactory.driver.set(new EdgeDriver());
-			break;
-		default:
-			throw new RuntimeException("Browser not supported: " + browser);
+			case "chrome":
+				ChromeOptions chromeOptions = new ChromeOptions();
+				if (isCI) {
+					 chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+				}
+				driver.set(new ChromeDriver(chromeOptions));
+				break;
+			case "edge":
+				EdgeOptions edgeOptions = new EdgeOptions();
+				if (isCI) {
+					edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--no-sandbox");
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+				}
+				driver.set(new EdgeDriver(edgeOptions));
+				break;
+			default:
+				throw new RuntimeException("Browser not supported: " + browser);
 		}
 		getDriver().manage().window().maximize();
 	}
